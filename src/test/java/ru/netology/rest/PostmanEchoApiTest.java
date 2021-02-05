4fb044c472f36cac41dbc819b5e0cca8c4eb41d3
Yuliya Kurbatova <j53781551@gmail.com>
2021-02-05 21:17:34 +0300

    Test failed

---
+++ src/test/java/ru/netology/rest/PostmanEchoApiTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoApiTest {
+    @Test
+    void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .header("content-type", "application/json")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
+
