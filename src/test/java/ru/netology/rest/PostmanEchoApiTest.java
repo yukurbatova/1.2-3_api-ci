fc8d0fa7e120dc7ac02a9f2a8416fca016ec151c
Yuliya Kurbatova <j53781551@gmail.com>
2021-02-05 21:24:55 +0300

    Test passed

--- src/test/java/ru/netology/rest/PostmanEchoApiTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoApiTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoApiTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
